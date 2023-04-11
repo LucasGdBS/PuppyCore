842fe0af212d8c4e69fd646fd86db81dccdb71f4
CarlosEdu <carlos.mec2005@gmail.com>
2023-04-11 12:22:26 -0300

    Alteracao no texto da home

--- projeto_puppy_core/app_puppy_core/models.py
+++ projeto_puppy_core/app_puppy_core/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
 # Create your models here.
 
 
-class Tutor(models.Model):  
+class Tutor(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     cpf = models.CharField(max_length=14, unique=True, null=False, blank=False)
     dataNascimento = models.DateField(max_length=10, blank=False)
