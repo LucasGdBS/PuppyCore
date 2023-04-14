333c43b24d3994bc3af4b3d422073a9bf997328a
Batatta <carlos.mec2005@gmail.com>
2023-04-14 12:54:56 -0300

    Apliquei o template nas páginas restantes e padronizei os forms, além de colocar a logo como um botao de voltar

--- projeto_puppy_core/projeto_puppy_core/urls.py
+++ projeto_puppy_core/projeto_puppy_core/urls.py
@@ -3,6 +3,7 @@ from app_puppy_core import views
 
 urlpatterns = [
     path('', views.home, name='Home'),
+    path('home/', views.home, name='Home'),
     path('cadastroTutor/', views.cadastroTutor, name='Cadastro Tutor'),
     path('falhacadastro/', views.falhacadastro, name='Falha Cadastro'),
     path('sucessocadastro/', views.sucessocadastro, name='Sucesso Cadastro'),
