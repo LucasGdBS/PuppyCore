7891ab8e3d86cd99648c830ca291cc8c65f480e7
Lucas-note <lgbs@cesar.school>
2023-04-16 19:24:47 -0300

    criacao static_root

--- projeto_puppy_core/projeto_puppy_core/settings.py
+++ projeto_puppy_core/projeto_puppy_core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = '/projeto_puppy_core/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
