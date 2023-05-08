64d0d3aadf3da78223e06444f3d9c54c9ff9306a
Lucas-note <lgbs@cesar.school>
2023-05-08 19:54:18 -0300

    atualização dos requirements e adicção do chromedriver

--- projeto_puppy_core/app_puppy_core/tests.py
+++ projeto_puppy_core/app_puppy_core/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
-# Create your tests here.
+#Teste rapido 
+class TesteTutor(TestCase):
+    def teste(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--icognito') 
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get('http://127.0.0.1:8000/')
+
+        login = driver.find_element(By.ID, 'login')
+        login.click()
+        sleep(2)
+        driver.quit()
+
