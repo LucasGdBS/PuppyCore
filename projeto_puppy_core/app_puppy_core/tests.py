76d2ea88395bd44b1bc98fa4815ec7a339382ae5
Lucas-note <lgbs@cesar.school>
2023-05-15 19:05:49 -0300

    testes tutor

--- projeto_puppy_core/app_puppy_core/tests.py
+++ projeto_puppy_core/app_puppy_core/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 def set_up():
@@ -13,119 +14,75 @@ def set_up():
         return driver
 
 class TesteTutor(TestCase):
-
-    def teste_cadastrar(self):
-        driver = set_up()
-        driver.get('http://127.0.0.1:8000/homeAdm/')
-        sleep(2)
-
-        
-        driver.find_element(By.ID, 'cadastrar_tutor').click()
-        
-        driver.find_element(By.ID, 'nome').send_keys('Homem de Ferro')
-        driver.find_element(By.ID, 'cpf').send_keys('123.864.286-88')
-        driver.find_element(By.ID, 'dataNascimento').send_keys('01071960')
-        driver.find_element(By.ID, 'celular').send_keys('(81)946271597')
-        driver.find_element(By.ID, 'email').send_keys('ironman@vingadores.com')
-        sleep(1)
-        driver.find_element(By.ID, 'enviar').click()
-        sleep(1)
-        driver.find_element(By.ID, 'confirmar').click()
-        sleep(1)        
-        driver.close()
-    
-    def teste_cadastro_cpf_repetido(self):
-        driver = set_up()
-        driver.get('http://127.0.0.1:8000/homeAdm/')
-        sleep(2)
-
-        for i in range(2):
-            driver.find_element(By.ID, 'cadastrar_tutor').click()
-            
-            driver.find_element(By.ID, 'nome').send_keys('Tanos')
-            driver.find_element(By.ID, 'cpf').send_keys('159.753.147-66')
-            driver.find_element(By.ID, 'dataNascimento').send_keys('01021973')
-            driver.find_element(By.ID, 'celular').send_keys('(81)915975324')
-            driver.find_element(By.ID, 'email').send_keys('thanos@joiadoinfinito.com')
-            sleep(1)
-            driver.find_element(By.ID, 'enviar').click()
-            sleep(1)
-            driver.find_element(By.ID, 'confirmar').click()
-            sleep(1)
-        driver.find_element(By.ID, "cancelar").click()
-        sleep(1)        
-        driver.close()
-    
-    def teste_cadastro_dado_faltando(self):
-        driver = set_up()
-        driver.get('http://127.0.0.1:8000/homeAdm/')
-
-        driver.find_element(By.ID, 'cadastrar_tutor').click()
-        driver.find_element(By.ID, 'nome').send_keys('Visão')
-        driver.find_element(By.ID, 'cpf').send_keys('178.654.258.44')
-        driver.find_element(By.ID, 'dataNascimento').send_keys('01101968')
-        driver.find_element(By.ID, 'celular').send_keys('(81)915975324')
-        driver.find_element(By.ID, 'enviar').click()
-        
-        driver.close()
-
-    # def teste_cadastro_email_incorreto(self): # Pode estar incorreto
-    #     driver = set_up()
-    #     driver.get('http://127.0.0.1:8000/homeAdm/')
-
-    #     driver.find_element(By.ID, 'cadastrar_tutor').click()
-    #     driver.find_element(By.ID, 'nome').send_keys('Visão')
-    #     driver.find_element(By.ID, 'cpf').send_keys('178.654.258.44')
-    #     driver.find_element(By.ID, 'dataNascimento').send_keys('01101968')
-    #     driver.find_element(By.ID, 'celular').send_keys('(81)915975324')
-    #     driver.find_element(By.ID, 'email').send_keys('visao@joiadamente')
-    #     driver.find_element(By.ID, 'enviar').click()
-    #     sleep(3)
-
-    #     driver.find_element(By.ID, 'enviar').click()
-
-    #     driver.close()
-    
-
-    def teste_add_pets(self):
-        pass
-    
-    def teste_add_faltando_dado(self):
-        pass
-
-    def teste_add_vacina(self):
-        pass
-
-    def teste_add_faltando_dado(self):
-        pass
-
-    def teste_ver_vacinas(self):
-        pass
     
     def teste_ver_tutores(self):
         driver = set_up()
-        driver.get('http://127.0.0.1:8000/homeAdm/')
+        driver.get('http://127.0.0.1:8000')
+        sleep(2)
         
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'cadastrar_tutor').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'nome').send_keys('NOME1')
+        driver.find_element(By.ID, 'cpf').send_keys('111.111.111-11')
+        driver.find_element(By.ID, 'dataNascimento').send_keys('01012001')
+        driver.find_element(By.ID, 'celular').send_keys('(81)111111111')
+        driver.find_element(By.ID, 'email').send_keys('email1@gmail.com')
+        sleep(2)
+        driver.find_element(By.ID, 'enviar').click()
+        sleep(2)
+        driver.find_element(By.ID, 'confirmar').click()
         sleep(2)
         
         elements = driver.find_elements(By.ID, 'nome_tutor')
+        for element in elements:
+            if element.text == 'NOME1':
+                expected_text = "NOME1"
+                assert element.text == expected_text
+                break      
+            
         
-        expected_text1 = "Homem de Ferro"
-        expected_text2 = "Tanos"
-
-        element1 = elements[0]
-        element2 = elements[1]
-        
-        assert element1.text == expected_text1 and element2.text == expected_text2
+        driver.close()
    
 
     def teste_ver_info_tutor(self):
         driver = set_up()
-        driver.get('http://127.0.0.1:8000/homeAdm/')
-        
+        driver.get('http://127.0.0.1:8000')
         sleep(2)
         
-        driver.find_element(By.ID, 'nome_tutor').click()
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'cadastrar_tutor').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'nome').send_keys('NOME2')
+        driver.find_element(By.ID, 'cpf').send_keys('222.222.222-22')
+        driver.find_element(By.ID, 'dataNascimento').send_keys('02022002')
+        driver.find_element(By.ID, 'celular').send_keys('(81)222222222')
+        driver.find_element(By.ID, 'email').send_keys('email2@gmail.com')
+        sleep(2)
+        driver.find_element(By.ID, 'enviar').click()
+        sleep(2)
+        driver.find_element(By.ID, 'confirmar').click()
+        sleep(2)
+        
+        
+        elements = driver.find_elements(By.ID, 'nome_tutor')
+        for element in elements:
+            if element.text == 'NOME2':
+                element.click()
+                break
+        
         
         sleep(2)
         
@@ -137,20 +94,173 @@ class TesteTutor(TestCase):
         
         sleep(2)
         
-        assert nome.text == "Nome: Homem de Ferro" and cpf.text == "CPF: 123.864.286-88" and data.text == "Data de nascimento: July 1, 1960"
-        assert celular.text == "Celular: (81)946271597" and email.text == "Email: ironman@vingadores.com"
+        assert nome.text == "Nome: NOME2" and cpf.text == "CPF: 222.222.222-22" and data.text == "Data de nascimento: Feb. 2, 2002"
+        assert celular.text == "Celular: (81)222222222" and email.text == "Email: email2@gmail.com"
         
-        
-        
+        driver.close()
 
     def teste_dados_alterar_preenchidos(self):
-        pass
+        driver = set_up()
+        driver.get('http://127.0.0.1:8000')
+        sleep(2)
+        
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'cadastrar_tutor').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'nome').send_keys('NOME3')
+        driver.find_element(By.ID, 'cpf').send_keys('333.333.333-33')
+        driver.find_element(By.ID, 'dataNascimento').send_keys('03032003')
+        driver.find_element(By.ID, 'celular').send_keys('(81)333333333')
+        driver.find_element(By.ID, 'email').send_keys('email3@gmail.com')
+        sleep(2)
+        driver.find_element(By.ID, 'enviar').click()
+        sleep(2)
+        driver.find_element(By.ID, 'confirmar').click()
+        sleep(2)
+        
+        elements = driver.find_elements(By.ID, 'nome_tutor')
+        for element in elements:
+            if element.text == 'NOME3':
+                element.click()
+                break
+        
+        sleep(2)
+        
+        driver.find_element(By.ID, 'alterar_tutor').click()
+        
+        sleep(2)
+        
+        nome = driver.find_element(By.ID, 'nome').get_attribute("value")
+        cpf = driver.find_element(By.ID, 'cpf').get_attribute("value")
+        data_nascimento = driver.find_element(By.ID, 'dataNascimento').get_attribute("value")
+        celular = driver.find_element(By.ID, 'celular').get_attribute("value")
+        email = driver.find_element(By.ID, 'email').get_attribute("value")
+       
+        assert nome == "NOME3" and cpf == "333.333.333-33" and data_nascimento == "2003-03-03"
+        assert celular == "(81)333333333" and email == "email3@gmail.com"
+        
+        driver.close()
     
     def teste_alterar_cadastro(self):
-        pass
+        driver = set_up()
+        driver.get('http://127.0.0.1:8000')
+        sleep(2)
+        
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'cadastrar_tutor').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'nome').send_keys('NOME4')
+        driver.find_element(By.ID, 'cpf').send_keys('444.444.444-45') #
+        driver.find_element(By.ID, 'dataNascimento').send_keys('04042005')#
+        driver.find_element(By.ID, 'celular').send_keys('(81)444444444')
+        driver.find_element(By.ID, 'email').send_keys('email5@gmail.com') #
+        sleep(2)
+        driver.find_element(By.ID, 'enviar').click()
+        sleep(2)
+        driver.find_element(By.ID, 'confirmar').click()
+        sleep(2)
+        
+        elements = driver.find_elements(By.ID, 'nome_tutor')
+        for element in elements:
+            if element.text == 'NOME4':
+                element.click()
+                break
+        sleep(2)
+        
+        driver.find_element(By.ID, 'alterar_tutor').click()
+        sleep(5)
+        
+        driver.find_element(By.ID, 'cpf').clear()
+        driver.find_element(By.ID, 'cpf').send_keys('444.444.444-44')
+        sleep(1)
+        
+        driver.find_element(By.ID, 'dataNascimento').clear()
+        driver.find_element(By.ID, 'dataNascimento').send_keys('04042004')
+        sleep(1)
+        
+        driver.find_element(By.ID, 'email').clear()
+        driver.find_element(By.ID, 'email').send_keys('email4@gmail.com')
+        sleep(1)
+        
+        driver.find_element(By.ID, 'enviar').click()
+        sleep(2)
+        driver.find_element(By.ID, 'confirmar').click()
+        sleep(2)
+        
+        assert driver.find_element(By.ID, 'cpf_tutor').text == "CPF: 444.444.444-44" and\
+        driver.find_element(By.ID, 'data_tutor').text == "Data de nascimento: April 4, 2004" and\
+        driver.find_element(By.ID, 'email_tutor').text == "Email: email4@gmail.com"
+        
+        driver.close()
     
     def teste_alterar_cadastro_faltando_dados(self):
-        pass
+        driver = set_up()
+        driver.get('http://127.0.0.1:8000')
+        sleep(2)
+        
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'login').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'cadastrar_tutor').click()
+        sleep(2)
+        
+        driver.find_element(By.ID, 'nome').send_keys('NOME5')
+        driver.find_element(By.ID, 'cpf').send_keys('555.555.555-56') #
+        driver.find_element(By.ID, 'dataNascimento').send_keys('05052006')#
+        driver.find_element(By.ID, 'celular').send_keys('(81)555555555')
+        driver.find_element(By.ID, 'email').send_keys('email6@gmail.com') #
+        sleep(2)
+        driver.find_element(By.ID, 'enviar').click()
+        sleep(2)
+        driver.find_element(By.ID, 'confirmar').click()
+        sleep(2)
+        
+        elements = driver.find_elements(By.ID, 'nome_tutor')
+        for element in elements:
+            if element.text == 'NOME5':
+                element.click()
+                break
+        sleep(2)
+        
+        driver.find_element(By.ID, 'alterar_tutor').click()
+        sleep(5)
+        
+        driver.find_element(By.ID, 'cpf').clear()
+        driver.find_element(By.ID, 'cpf').send_keys('555.555.555-55')
+        sleep(1)
+        
+        driver.find_element(By.ID, 'dataNascimento').clear()
+        driver.find_element(By.ID, 'dataNascimento').send_keys('05052005')
+        sleep(1)
+        
+        driver.find_element(By.ID, 'email').clear()
+        sleep(1)
+        
+        count = 0;
+        
+        driver.find_element(By.ID, 'email').send_keys(Keys.ENTER)
+        count +=1
+        driver.find_element(By.ID, 'email').send_keys(Keys.ENTER)
+        count +=1
+        
+        assert count == 2
+        
+        driver.close() 
 
     def teste_ver_pets(self):
         driver = set_up()
