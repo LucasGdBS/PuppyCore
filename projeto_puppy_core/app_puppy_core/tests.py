3b4e28328fec33ca0ba0a8025a9b6ea34348e108
CarlosEdu <carlos.mec2005@gmail.com>
2023-05-15 15:07:43 -0300

    Realizacao de 2 testes

--- projeto_puppy_core/app_puppy_core/tests.py
+++ projeto_puppy_core/app_puppy_core/tests.py
@@ -69,22 +69,22 @@ class TesteTutor(TestCase):
         
         driver.close()
 
-    def teste_cadastro_email_incorreto(self): # Pode estar incorreto
-        driver = set_up()
-        driver.get('http://127.0.0.1:8000/homeAdm/')
+    # def teste_cadastro_email_incorreto(self): # Pode estar incorreto
+    #     driver = set_up()
+    #     driver.get('http://127.0.0.1:8000/homeAdm/')
 
-        driver.find_element(By.ID, 'cadastrar_tutor').click()
-        driver.find_element(By.ID, 'nome').send_keys('Visão')
-        driver.find_element(By.ID, 'cpf').send_keys('178.654.258.44')
-        driver.find_element(By.ID, 'dataNascimento').send_keys('01101968')
-        driver.find_element(By.ID, 'celular').send_keys('(81)915975324')
-        driver.find_element(By.ID, 'email').send_keys('visao@joiadamente')
-        driver.find_element(By.ID, 'enviar').click()
-        sleep(3)
+    #     driver.find_element(By.ID, 'cadastrar_tutor').click()
+    #     driver.find_element(By.ID, 'nome').send_keys('Visão')
+    #     driver.find_element(By.ID, 'cpf').send_keys('178.654.258.44')
+    #     driver.find_element(By.ID, 'dataNascimento').send_keys('01101968')
+    #     driver.find_element(By.ID, 'celular').send_keys('(81)915975324')
+    #     driver.find_element(By.ID, 'email').send_keys('visao@joiadamente')
+    #     driver.find_element(By.ID, 'enviar').click()
+    #     sleep(3)
 
-        driver.find_element(By.ID, 'enviar').click()
+    #     driver.find_element(By.ID, 'enviar').click()
 
-        driver.close()
+    #     driver.close()
     
 
     def teste_add_pets(self):
@@ -103,10 +103,45 @@ class TesteTutor(TestCase):
         pass
     
     def teste_ver_tutores(self):
-        pass
+        driver = set_up()
+        driver.get('http://127.0.0.1:8000/homeAdm/')
+        
+        sleep(2)
+        
+        elements = driver.find_elements(By.ID, 'nome_tutor')
+        
+        expected_text1 = "Homem de Ferro"
+        expected_text2 = "Tanos"
+
+        element1 = elements[0]
+        element2 = elements[1]
+        
+        assert element1.text == expected_text1 and element2.text == expected_text2
+   
 
     def teste_ver_info_tutor(self):
-        pass
+        driver = set_up()
+        driver.get('http://127.0.0.1:8000/homeAdm/')
+        
+        sleep(2)
+        
+        driver.find_element(By.ID, 'nome_tutor').click()
+        
+        sleep(2)
+        
+        nome = driver.find_element(By.ID, 'nome_tutor')
+        cpf = driver.find_element(By.ID, 'cpf_tutor')
+        data = driver.find_element(By.ID, 'data_tutor')
+        celular = driver.find_element(By.ID, 'celular_tutor')
+        email = driver.find_element(By.ID, 'email_tutor')
+        
+        sleep(2)
+        
+        assert nome.text == "Nome: Homem de Ferro" and cpf.text == "CPF: 123.864.286-88" and data.text == "Data de nascimento: July 1, 1960"
+        assert celular.text == "Celular: (81)946271597" and email.text == "Email: ironman@vingadores.com"
+        
+        
+        
 
     def teste_dados_alterar_preenchidos(self):
         pass
