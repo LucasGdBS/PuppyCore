9b5cf49cb4f237d4cb5d3f7a05e7a78d1c8a5ea0
Lucas-note <lgbs@cesar.school>
2023-06-03 15:04:34 -0300

    mais opcoes no teste

--- projeto_puppy_core/app_puppy_core/tests.py
+++ projeto_puppy_core/app_puppy_core/tests.py
@@ -6,10 +6,13 @@ from time import sleep
 
 
 def set_up():
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--icognito')
-    driver = webdriver.Edge()
-    driver.maximize_window()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--icognito')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome_options)
+    # driver.maximize_window()
 
     return driver
 
