5579b65cdb88d5922d091a784ed9075a2be888c7
Lucas-note <lgbs@cesar.school>
2023-06-03 16:42:12 -0300

    add: arguments for tests

--- projeto_puppy_core/app_puppy_core/tests.py
+++ projeto_puppy_core/app_puppy_core/tests.py
@@ -8,9 +8,9 @@ from time import sleep
 def set_up():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--icognito')
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
 
